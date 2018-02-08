b3ea4f1d227ff7bc3a7fa74f2ef751ab7847eb90
Carlos Sanchez <csanchezg@viewnext.com>
2018-02-08 19:16:37 +0100

    Muy bien

--- src/javaProject/Hola.java
+++ src/javaProject/Hola.java
@@ -5,6 +5,7 @@ public class Hola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola que tal");
+		System.out.println("Muy bien");
 	}
 
 }
